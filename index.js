5d3243f7196beb403d8cf58c489da1b10ae2e682
Jared Forsyth <jared@jaredforsyth.com>
2013-06-04 01:38:41 -0600

    making it really work; had to work around a recursive issue

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 
-var template = require('template');
+var template = require('./template');
 
 var ng = {
-  deps: {'tags': require('tags')},
-  directive: {
-    scope: {},
-    replace: true,
-    restrict: 'A',
-    template: template,
-    link: function (scope, element, attrs) {
-      var name = attrs.note;
-      var localName = 'note';
-      scope.$parent.$watch(name, function(value) {
-        scope[localName] = value;
-      });
-      scope.$watch(localName, function(value) {
-        scope.$parent[name] = value;
-      });
-    }
-  }
+    deps: {'tags': require('tags')},
+    directive: ['$compile',
+        function($compile){
+    return {
+      scope: {},
+      replace: true,
+      restrict: 'A',
+      // template: template,
+      // terminal: true,
+      link: function (scope, element, attrs) {
+        var name = attrs.note;
+        var localName = 'note';
+        element.html(template);
+        $compile(element.contents())(scope);
+        scope.$parent.$watch(name, function(value) {
+          scope[localName] = value;
+        });
+        scope.$watch(localName, function(value) {
+          scope.$parent[name] = value;
+        });
+      }
+    };
+  }]
 };
 
 module.exports = ng;
